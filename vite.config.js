27406f37118815ab454c03765ee09b31161b6413
Trevor Danner <tjdanner01@gmail.com>
2024-11-23 14:00:24 -0600

    Fix MIME type issue and update Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/mended-repair-app-2/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+  },
+  server: {
+    mimeTypes: {
+      "application/javascript": ["js"],
+    },
+  },
 });
